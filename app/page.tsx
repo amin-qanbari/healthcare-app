4ff018388fb10518cc16c3c53f84b691b514ab6c
amin <aminqanbarii1992@gmail.com>
2024-07-22 00:39:49 +0330

    work on paakeyModal

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
-import PatientForm from '@/components/forms/PatientForm';
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
-import Link from 'next/link';
+import PatientForm from "@/components/forms/PatientForm";
+import PasskeyModal from "@/components/PasskeyModal";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+export default function Home({ searchParams }: SearchParamProps) {
+  const isAdmin = searchParams.admin === "true";
+
   return (
     <div className="flex h-screen max-h-screen">
+      {isAdmin && <PasskeyModal/>}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
@@ -27,13 +31,13 @@ export default function Home() {
           </div>
         </div>
       </section>
-            <Image
-              src="/assets/images/onboarding-img.png"
-              height={1000}
-              width={1000}
-              alt="patient"
-              className="side-img max-w-[50%]"
-            />
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
